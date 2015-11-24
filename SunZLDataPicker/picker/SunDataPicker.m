368b55df50cdb2dce7faa4e9f9c8e8f4e1ea471f
sunzl <sunzl@sunzldeimac.local>
2015-11-24 14:49:29 +0800

    testBranch

--- SunZLDataPicker/picker/SunDataPicker.m
+++ SunZLDataPicker/picker/SunDataPicker.m
@@ -38,6 +38,7 @@
     _title.textAlignment=NSTextAlignmentCenter;
     _title.text=@"请选择";
     //test pull push commit
+    //test master
     _picker=[[UIPickerView alloc]initWithFrame:CGRectMake(10,0, frame.size.width-20, frame.size.height)];
     _picker.delegate=self;
     _picker.dataSource=self;
